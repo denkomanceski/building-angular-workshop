fb3f026b14f762d29303c5a339ae5623282b266f
Denko Manceski <denkomanceski@gmail.com>
2017-04-25 15:35:50 +0200

    future module

--- src/app/future/future.module.ts
+++ src/app/future/future.module.ts
@@ -3,13 +3,12 @@ import {CommonModule} from '@angular/common';
 import {TestComponent} from './test/test.component';
 import {HelloComponent} from './hello/hello.component';
 import {GoodbyeComponent} from './goodbye/goodbye.component';
-import {Future2Module} from '../future2/future2.module';
+
 
 
 @NgModule({
     imports: [
-        CommonModule,
-        Future2Module
+        CommonModule
     ],
     declarations: [
         TestComponent,
